95bc00e52ba030f1bdea382a44da19eee2cc189e
Helena Kaikkonen <helenak@uw.edu>
2023-04-20 19:11:46 -0700

    removed unnecessary imports

--- Trial 2.0/index.js
+++ Trial 2.0/index.js
@@ -1,31 +1,32 @@
-import {client_id, client_secret, redirect_uri, scope, AUTHORIZE, handleRedirect} from "./spotify.js";
+import { client_id, client_secret, redirect_uri, scope, AUTHORIZE, handleRedirect } from "./spotify.js";
 
 var access_token = null;
 
 document.addEventListener("DOMContentLoaded", onPageLoad);
 
 function onPageLoad() {
-console.log("index.js onPageLoad");
-if (window.location.search.length > 0) {
-    handleRedirect();
-    console.log("handled redirect!");
-} else {
-    access_token = localStorage.getItem("access_token");
-    if (access_token == null) {
-        // we don't have an access token so present token section
-        console.log("No access token.");
-        document.getElementById("tokenSection").style.display = "block";
+    document.getElementById('authorizeBtn').addEventListener('click', requestAuthorization);
+    console.log("index.js onPageLoad");
+    if (window.location.search.length > 0) {
+        handleRedirect();
+        console.log("handled redirect!");
     } else {
-        console.log("Has access Token")
-        // we have an access token so present device section
-        console.log("Access token: " + access_token);
-        // document.getElementById("deviceSection").style.display = "block";
-        // refreshTopTracks();
-        // refreshDevices();
-        // currentlyPlaying();
-        window.location.replace("/game");
+        access_token = localStorage.getItem("access_token");
+        if (access_token == null) {
+            // we don't have an access token so present token section
+            console.log("No access token.");
+            document.getElementById("tokenSection").style.display = "block";
+        } else {
+            console.log("Has access Token")
+            // we have an access token so present device section
+            console.log("Access token: " + access_token);
+            // document.getElementById("deviceSection").style.display = "block";
+            // refreshTopTracks();
+            // refreshDevices();
+            // currentlyPlaying();
+            window.location.replace("/game");
+        }
     }
-}
 }
 
 //requestAuthorization
@@ -44,4 +45,3 @@ function requestAuthorization() {
     url += "&scope=" + scope;
     window.location.href = url; // Show Spotify's authorization screen
 }
-document.getElementById('authorizeBtn').addEventListener('click', requestAuthorization);
