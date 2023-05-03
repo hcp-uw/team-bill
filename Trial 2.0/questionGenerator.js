0520930448d1d50fb66fdd30769daedc3a177dff
Zack <zacktcrouse@gmail.com>
2023-05-03 13:53:08 -0700

    Worked on questionGen setAnswer functionality.
    Co-authored-by: helena7802 <helena7802@users.noreply.github.com>
    Co-authored-by: KristenGus42 <KristenGus42@users.noreply.github.com>

--- Trial 2.0/questionGenerator.js
+++ Trial 2.0/questionGenerator.js
@@ -52,56 +52,84 @@ class simpleQuestionGen {
      * Set answer and non answer fields to correct values
      */
     setAnswer() {
-        var maxRange = this.curQuestion.max;
-        
-        console.log(this.curQuestion)
-        console.log(this.curQuestion.apiCall);
+        console.log("Set Answer Below \n ------------------------");
+        let maxRange = this.curQuestion.max;
+        let number = 0;
+
+        console.log("Current Question:")
+        console.log(this.curQuestion);
         const items = this.apiResponseMap.get(this.curQuestion.apiCall).items;
+
+        // if maxRange is -1, set to 20
+        // if maxRange is not -1. set "number" to pseudo-random number within range (greater chance for lower numbers)
+        // if items.length is less than maxRange, set maxRange to items.length
         if (maxRange === -1) {
-            var number = 1;
-            maxRange = 20;
-            
-            if(items.length < 20) {
-                this.curQuestion.max = items.length;
-            }  
+            number = 1;
+            maxRange = Math.min(items.length, 20);
         } else {
-            if(items.length < 20) {
-                this.curQuestion.max = items.length;
-            }
-            var number = Math.floor(Math.pow((Math.random() * maxRange), 2) / (maxRange)) + 1;
+            maxRange = Math.min(items.length, maxRange);
+            number = Math.floor(Math.pow((Math.random() * maxRange), 2) / (maxRange)) + 1;
         }
-        console.log(number);
+        console.log("Number:" + number);
+        this.curQuestion.question = this.curQuestion.question.replace("_", number); // add chosen random number to question if necessary
+        console.log(this.curQuestion);
         
         const id = this.curQuestion.id;
 
+        // reset current answer & non-answers
         this.curAnswer = this.findAnswer(id, number);
         this.curNonAnswers = [];
 
-        var usedNumbers = [number];
+        let usedNumbers = [number];
         while (this.curNonAnswers.length < 3) {
+            console.log("(WHILE LOOP) Used numbers so far:");
+            console.log(usedNumbers);
             let offNumber = Math.floor(Math.random() * maxRange) + 1;
             if (!usedNumbers.includes(offNumber)) {
                 this.curNonAnswers.push(this.findAnswer(id, offNumber));
                 usedNumbers.push(offNumber);
             }
         }
+
+        console.log("Current answer:");
+        console.log(this.curAnswer);
+        console.log("Current NonAnsers:");
+        console.log(this.curNonAnswers);
+        console.log("------------------------");
     }
 
     /**
-     * Uses the information from the spotify api to answer a given question.
+     * Uses the information from the Spotify API to return the answer to a given question.
      * @param {number} questionID ID of the question to get the answer for.
      * @param {number} number The numeric modifier to the question.
-     * @returns {string} Returns the answer to the question.
+     * @returns {string} Returns the answer to the question as a string.
      */
     findAnswer(questionID, number) {
         // TODO: add code here
+
+        switch (questionID) {
+            case 1:
+                let song = this.apiResponseMap.get("track-long-50").items[number-1];
+                return song.name;
+            case 2:
+                // TODO
+            case 3: 
+                // TODO
+            case 4:
+                // TODO
+        }
+        
+        return "ID:" + questionID +  ", number: " + number;
     }
 
     /**
-     * Picks a question from questions at random and removes it.
+     * Sets this.curQuestion to a question from this.questions and removes that question from the list.
      */
     pickQuestion() {
-        if(this.questions !== undefined) {
+        if (this.questions.length === 0) {
+            throw new Error("QuestionGen: There are no questions left in this.questions");
+        }
+        if (this.questions !== undefined) {
             this.curQuestion = this.questions.splice(Math.floor(Math.random() * this.questions.length), 1)[0];
         }
     }
