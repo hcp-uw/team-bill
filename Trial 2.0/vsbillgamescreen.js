c16e272f0cce0aa45ef0c9894089bdcda49d8b87
queenofyourtown <115186410+queenofyourtown@users.noreply.github.com>
2023-05-23 12:24:28 -0700

    Text box and adding values to js array

---
+++ Trial 2.0/vsbillgamescreen.js
@@ -0,0 +1,9 @@
+//size 10 array with answers from game mode
+const billanswers [];
+
+function addAnswers() {
+    var ans1 = document.getElementById('q1').value 
+    billanswers.push(ans1);
+    console.log(billanswers);
+    return false;
+}
