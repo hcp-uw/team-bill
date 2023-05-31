efe6c0ea35a841ab649fee7b5e1a3f7d380b2a01
Colin Lim <colinlim@uw.edu>
2023-05-30 17:15:22 -0700

    changed answer types to IDs and edited pointer css

--- Trial 2.0/sologamescreen.js
+++ Trial 2.0/sologamescreen.js
@@ -1,7 +1,5 @@
 import {makeQuestionGen} from "./questionGenerator.js";
 
-// [top-left, top-right, bottom-left, bottom-right] answer buttons respectively
-let correctAnswer = new Array(4); 
 // Current question number
 let questionNumber = 1;
 // Current number of correct answers
@@ -10,8 +8,6 @@ let score = 0;
 let gen;
 makeQuestionGen().then(function(value) {
     gen = value;
-    console.log("Inside makeQuestionGen.then")
-    console.log(value);
     onLoad();
 }) 
 
@@ -19,15 +15,14 @@ makeQuestionGen().then(function(value) {
 function onLoad() {
     loadText();
 
-    document.getElementsByClassName('answer-top-left')[0].addEventListener('click', ( () => checkAnswer('answer-top-left')));
-    document.getElementsByClassName('answer-top-right')[0].addEventListener('click', ( () => checkAnswer('answer-top-right')));
-    document.getElementsByClassName('answer-bottom-left')[0].addEventListener('click', ( () => checkAnswer('answer-bottom-left')));
-    document.getElementsByClassName('answer-bottom-right')[0].addEventListener('click', ( () => checkAnswer('answer-bottom-right')));
+    document.getElementById('answer-top-left').addEventListener('click', ( () => checkAnswer('answer-top-left')));
+    document.getElementById('answer-top-right').addEventListener('click', ( () => checkAnswer('answer-top-right')));
+    document.getElementById('answer-bottom-left').addEventListener('click', ( () => checkAnswer('answer-bottom-left')));
+    document.getElementById('answer-bottom-right').addEventListener('click', ( () => checkAnswer('answer-bottom-right')));
 }
 
 // Replaces the question and all 4 answer choices with their respective text. Resets corret answer
 function loadText() {
-    
     document.getElementById("question").innerText = gen.getQuestion();
     document.getElementById("question-number").innerText = questionNumber;
     loadAnswers();
@@ -36,51 +31,33 @@ function loadText() {
 // Replaces all answer text with 3 wrong answers and 1 correct answer chosen randomly. Changes the
 // global correct answer boolean to correctly represent right and wrong answers
 function loadAnswers() {
-    // the first element of this array represents the correct answer
-    correctAnswer = [false, false, false, false]; //top-left, top-right, bottom-left, bottom-right 
-    // let arrAnswers = new Array("1","2","3","4"); // (replace with function calls later for answers)
     let arrAnswers = [gen.getAnswer(), ...gen.getNonAnswers()];
-    console.log(arrAnswers);
-
     let btnIDs = ["answer-top-left", "answer-top-right", "answer-bottom-left", "answer-bottom-right"];
 
     for (let i = 0; i < 4; i++) {
         const rand = getRandomInt(arrAnswers.length); // Random index of arrAnswers
-        console.log(rand);
-        const btn = document.getElementsByClassName(btnIDs.pop())[0];
+        const btn = document.getElementById(btnIDs.pop());
         if (arrAnswers[rand] === gen.getAnswer()) {
-            correctAnswer[btnIDs.length] = true; 
             // Since the corresponding index of correctAnswers is the index of btnIDs and we just poped
             // the current btnID that means that btnIDS.length is the index of btn.
         }
         btn.innerText = arrAnswers.splice(rand, 1)[0];
     }
-    console.log(correctAnswer);
 }
 
-// Returns a random int between 0-max exclusive
+// Returns a random int between 0 to (max - 1) inclusive
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
 // Checks if clicked button was the correct answer
 function checkAnswer(name) {
-    if (name === "answer-top-left" && correctAnswer[0] === true) {
-        alert("you got it right");
-        score++;
-    } else if (name === "answer-top-right" && correctAnswer[1] === true) {
-        alert("you got it right");
-        score++;
-    } else if (name === "answer-bottom-left" && correctAnswer[2] === true) {
-        alert("you got it right");
-        score++;
-    } else if (name === "answer-bottom-right" && correctAnswer[3] === true) {
+    if (document.getElementById(name).innerText === gen.getAnswer()) {
         alert("you got it right");
         score++;
     } else {
         alert("wrong");
     }
-
     // Goes to the next question or final screen
     questionNumber++;
     if (questionNumber <= 10) {
