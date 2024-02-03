1a650ba804ccc4eeafc5d826cd93feeee82edc70
Zack Crouse <zacktcrouse@gmail.com>
2024-02-02 20:31:55 -0800

    Changed where api key is stored for easy of use.

---
+++ public/api_key.js
@@ -0,0 +1,5 @@
+// Replace keys with your own. You can create an API key here: https://developer.spotify.com/dashboard
+// If you do not use your own api key then you will not be able to log into your Spotify account.
+// Make sure to use the email associated with your Spotify account when making the api key.
+export var client_id = "a0c734380e8a4301b8af9f29b139165c";
+export var client_secret = "33dad7cfdcd347b0a83d551537ffa728";
--- public/spotify.js
+++ public/spotify.js
@@ -1,5 +1,4 @@
-export var client_id = "a0c734380e8a4301b8af9f29b139165c";
-export var client_secret = "33dad7cfdcd347b0a83d551537ffa728"; // In a real app you should not expose your client_secret to the user
+import {client_id, client_secret} from "./api_key.js";
 export var scope = "user-top-read user-read-recently-played";
 
 var redirect_uri;
